a64f73c6246df3f9e447026867de78586df91fee
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-07-01 23:06:09 +0700

    Update functions.js

--- server/cloud/functions.js
+++ server/cloud/functions.js
@@ -1,14 +1,22 @@
-Parse.Cloud.define('hello', req => {
-  req.log.info(req);
-  return 'Hi';
+Parse.Cloud.define("hello", (req) => {
+    req.log.info(req);
+    return "Hi";
 });
 
-Parse.Cloud.define('asyncFunction', async req => {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  req.log.info(req);
-  return 'Hi async';
+Parse.Cloud.define("asyncFunction", async (req) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    req.log.info(req);
+    return "Hi async";
 });
 
-Parse.Cloud.beforeSave('Test', () => {
-  throw new Parse.Error(9001, 'Saving test objects is not available.');
+Parse.Cloud.beforeSave("Test", () => {
+    throw new Parse.Error(9001, "Saving test objects is not available.");
+});
+
+Parse.Cloud.afterSave(Parse.User, (request) => {
+    // https://stackoverflow.com/questions/53325756/parse-server-cloud-code-aftersave-trigger
+    if (!request.original) {
+        console.log("object: ", request);
+        console.log("user:", user);
+    }
 });
