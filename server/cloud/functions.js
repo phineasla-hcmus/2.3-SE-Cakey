ccc37eedce893e1ffcc4c8cae1ff94cf6a02ad10
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-07-03 00:17:42 +0700

    Update functions.js

--- server/cloud/functions.js
+++ server/cloud/functions.js
@@ -13,26 +13,25 @@ Parse.Cloud.beforeSave("Test", () => {
     throw new Parse.Error(9001, "Saving test objects is not available.");
 });
 
-Parse.Cloud.afterSave(Parse.User, (request) => {
+Parse.Cloud.afterSave(Parse.User, async (request) => {
     // https://stackoverflow.com/questions/53325756/parse-server-cloud-code-aftersave-trigger
     if (!request.original) {
         const userQuery = new Parse.Query(Parse.User).get;
         const iconQuery = new Parse.Query("ProfileIcon").get;
-        Parse.Config.get().then(function(config) {
-            const userId = request.object.id;
-            const iconId = config.get("defaultProfileIcon");
-            try {
-                let [user, icon] = await Promise.all([
-                    userQuery(userId, { useMasterKey: true }),
-                    iconQuery(iconId),
-                ]);
-                user.set("level", 1);
-                user.set("exp", 0);
-                user.set("profileIcon", icon);
-                user.save(null, { useMasterKey: true });
-            } catch (error) {
-                console.error(error);
-            }
-        });
+        const config = await Parse.Config.get();
+        const userId = request.object.id;
+        const iconId = config.get("defaultProfileIcon");
+        try {
+            let [user, icon] = await Promise.all([
+                userQuery(userId, { useMasterKey: true }),
+                iconQuery(iconId),
+            ]);
+            user.set("level", 1);
+            user.set("exp", 0);
+            user.set("profileIcon", icon);
+            user.save(null, { useMasterKey: true });
+        } catch (error) {
+            console.error(error);
+        }
     }
 });
