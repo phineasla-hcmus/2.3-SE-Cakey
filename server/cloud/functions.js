1de0c2cfdd8fb1adabe849391963d6ec0d2459b2
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-07-16 00:29:24 +0700

    revert

--- server/cloud/functions.js
+++ server/cloud/functions.js
@@ -13,50 +13,48 @@ Parse.Cloud.beforeSave("Test", () => {
     throw new Parse.Error(9001, "Saving test objects is not available.");
 });
 
-// Parse.Cloud.afterSave(Parse.User, async (request) => {
-//     // https://stackoverflow.com/questions/53325756/parse-server-cloud-code-aftersave-trigger
-//     if (!request.original) {
-//         const userQuery = new Parse.Query(Parse.User);
-//         const iconQuery = new Parse.Query("ProfileIcon");
-//         const config = await Parse.Config.get();
-//         const userId = request.object.id;
-//         const iconId = config.get("defaultProfileIcon");
-//         try {
-//             let [user, icon] = await Promise.all([
-//                 userQuery.get(userId, { useMasterKey: true }),
-//                 iconQuery.get(iconId),
-//             ]);
-//             user.set("level", 1);
-//             user.set("exp", 0);
-//             user.set("profileIcon", icon);
-//             user.save(null, { useMasterKey: true });
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
-// });
-
-Parse.Cloud.beforeSave(Parse.User, (request) => {
+Parse.Cloud.afterSave(Parse.User, async (request) => {
+    // https://stackoverflow.com/questions/53325756/parse-server-cloud-code-aftersave-trigger
     if (!request.original) {
-        // New user
-        console.log("Start edit User");
-        Parse.Config.get()
-            .then((config) => {
-                const ProfileIcon = Parse.Object.extend("ProfileIcon");
-                const iconId = config.get("defaultProfileIcon");
-                const user = request.object;
-                const useMasterKey = { useMasterKey: true };
-                user.set("level", 1, useMasterKey);
-                user.set("exp", 0, useMasterKey);
-                user.set(
-                    "profileIcon",
-                    ProfileIcon.createWithoutData(iconId),
-                    useMasterKey
-                );
-                console.log("DONE EDIT");
-            })
-            .catch((err) => {
-                console.error(err);
-            });
+        const userQuery = new Parse.Query(Parse.User);
+        const iconQuery = new Parse.Query("ProfileIcon");
+        const config = await Parse.Config.get();
+        const userId = request.object.id;
+        const iconId = config.get("defaultProfileIcon");
+        try {
+            let [user, icon] = await Promise.all([
+                userQuery.get(userId, { useMasterKey: true }),
+                iconQuery.get(iconId),
+            ]);
+            user.set("level", 1);
+            user.set("exp", 0);
+            user.set("profileIcon", icon);
+            user.save(null, { useMasterKey: true });
+        } catch (error) {
+            console.error(error);
+        }
     }
 });
+
+// Parse.Cloud.beforeSave(Parse.User, (request, respone) => {
+//     if (!request.original) {
+//         // New user
+//         Parse.Config.get()
+//             .then((config) => {
+//                 const ProfileIcon = Parse.Object.extend("ProfileIcon");
+//                 const iconId = config.get("defaultProfileIcon");
+//                 const user = request.object;
+//                 const useMasterKey = { useMasterKey: true };
+//                 user.set("level", 1, useMasterKey);
+//                 user.set("exp", 0, useMasterKey);
+//                 user.set(
+//                     "profileIcon",
+//                     ProfileIcon.createWithoutData(iconId),
+//                     useMasterKey
+//                 );
+//             })
+//             .catch((err) => {
+//                 console.error(err);
+//             });
+//     }
+// });
