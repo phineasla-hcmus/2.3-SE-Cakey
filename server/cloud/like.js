8f321f871d5be1e00ccfa514197cdf6418c7e7ba
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-08-25 18:32:04 +0700

    fix like/dislike

--- server/cloud/like.js
+++ server/cloud/like.js
@@ -7,26 +7,27 @@ const Blog = Parse.Object.extend("Blog");
  */
 Parse.Cloud.define(
     "reactBlog",
-    async (req) => {
-        const { user, params } = req;
+    async ({ user, master, params }) => {
         const opts = master
             ? { useMasterKey: true }
             : { sessionToken: user.getSessionToken() };
+        const blogPointer = Blog.createWithoutData(params.blogId);
         // Query first to check if Like existed
         const queryLike = new Parse.Query("Like");
-        queryLike.equalTo("blog", params.blogId);
+        queryLike.equalTo("blog", blogPointer);
         queryLike.equalTo("user", user);
         let like = await queryLike.first(opts);
         let attrs = { type: params.type };
         if (!like) {
+            console.log("NEW LIKE, CREATING ONE NOW");
             like = new Like();
             attrs.user = user;
-            attrs.blog = Blog.createWithoutData(params.blogId);
+            attrs.blog = blogPointer;
         }
         // Bypass no-write CLP
         await like.save(attrs, {
             useMasterKey: true,
-            sessionToken: user.getSessionToken(),
+            // sessionToken: user.getSessionToken(),
         });
     },
     {
@@ -49,12 +50,12 @@ Parse.Cloud.define(
  */
 Parse.Cloud.define(
     "destroyReactBlog",
-    async (req) => {
+    async ({ params }) => {
         const queryLike = new Parse.Query("Like");
-        queryLike.equalTo("blog", req.params.blogId);
+        queryLike.equalTo("blog", params.blogId);
         let like = await queryLike.first();
         if (like === undefined)
-            throw `Like/Dislike not found for Blog ${req.params.blogId}`;
+            throw `Like/Dislike not found for Blog ${params.blogId}`;
         await like.destroy();
     },
     {
@@ -72,34 +73,39 @@ Parse.Cloud.define(
  */
 Parse.Cloud.beforeSave(
     "Like",
-    (req) => {
-        const { original, object, user } = req;
+    async ({ original, object }) => {
+        const user = object.get("user");
+        // Not the prettiest but it gets the job done
         const keys = ["dislike", "like"];
         const counters = [0, 0];
         if (object.isNew()) {
             object.setACL(utils.authorACL(user));
         } else {
-            counters[original.get("type")]--;
+            counters[original.get("type") ? 1 : 0]--;
         }
-        counters[object.get("type")]++;
-        // const blog = await object.get("blog");
-        // if (!blog) throw `Invalid Blog for Like ${object.id}`;
-        // for (let i = 0; i < counters.length; i++) {
-        //     if (counters[i] > 0) blog.increment(keys[i]);
-        //     else if (counters[i] < 0) blog.decrement(keys[i]);
-        // }
-        // blog.save(null, { useMasterKey: true });
-        object
+        counters[object.get("type") ? 1 : 0]++;
+        const blog = await object
             .get("blog")
-            .fetch({ sessionToken: user.getSessionToken() })
-            .then((blog) => {
-                for (let i = 0; i < counters.length; i++) {
-                    if (counters[i] > 0) blog.increment(keys[i]);
-                    else if (counters[i] < 0) blog.decrement(keys[i]);
-                }
-                return blog.save(null, { useMasterKey: true });
-            })
-            .catch(console.error);
+            ?.fetch({ sessionToken: user.getSessionToken() });
+        if (blog) {
+            for (let i = 0; i < counters.length; i++) {
+                if (counters[i] > 0) blog.increment(keys[i]);
+                else if (counters[i] < 0) blog.decrement(keys[i]);
+            }
+            await blog.save(null, { useMasterKey: true });
+        } else throw `Invalid Blog for Like ${object.id}`;
+
+        // object
+        //     .get("blog")
+        //     ?.fetch({ sessionToken: user.getSessionToken() })
+        //     .then((blog) => {
+        //         for (let i = 0; i < counters.length; i++) {
+        //             if (counters[i] > 0) blog.increment(keys[i]);
+        //             else if (counters[i] < 0) blog.decrement(keys[i]);
+        //         }
+        //         return blog.save(null, { useMasterKey: true });
+        //     })
+        //     .catch(console.error);
     },
     {
         fields: {
@@ -107,7 +113,7 @@ Parse.Cloud.beforeSave(
             blog: { required: true, constant: true },
             type: { required: true },
         },
-        requireUser: true,
+        // requireUser: true,
         requireMaster: true,
     }
 );
@@ -117,11 +123,11 @@ Parse.Cloud.beforeSave(
  * CLP write is disabled for all user to prevent multiple likes
  * (unique index is not supported in Parse 4.5.0 (Dec 15, 2020))
  */
-Parse.Cloud.afterDelete("Like", async (req) => {
-    const { object } = req;
-    const blog = await object.get("blog");
-    if (blog === undefined) throw `Invalid Blog for Like ${object.id}`;
-    if (object.get("type")) blog.decrement("like");
-    else blog.decrement("dislike");
-    blog.save(null, { useMasterKey: true });
-});
+// Parse.Cloud.afterDelete("Like", async ({ object }) => {
+//     // TODO: fetch after get
+//     const blog = await object.get("blog");
+//     if (blog === undefined) throw `Invalid Blog for Like ${object.id}`;
+//     if (object.get("type")) blog.decrement("like");
+//     else blog.decrement("dislike");
+//     blog.save(null, { useMasterKey: true });
+// });
